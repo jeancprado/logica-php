b74c95ffbaec878657e3d5c22c8545bf03a1dd52
jean <jean.akaji@gmail.com>
2025-06-04 09:14:17 -0300

    enviando exercicio1 parte frontend

---
+++ exercicio1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <div class="bg-black">
+     <div class="container">
+    <h5>Exercício 01<h5>
+    <br>
+    <form action="index.php" method="post">
+        <div>
+        <label for="first number">Digite o primeiro número</label>
+        <input type="text" name="first number">
+        </div>
+        <br>
+        <div>
+        <label for="second number">Digite o segundo número</label>
+        <input type="text" name="second number">
+        </div>
+        <br>    
+        <input type="submit" value="enviar">
+    </form>
+
+</body>
+</html>
