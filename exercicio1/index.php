b3629f624c4d07181cb73ecfbff26a5807c21d2c
jean <jean.akaji@gmail.com>
2025-06-18 09:00:54 -0300

    corrigindo identação do index-php

--- exercicio1/index.php
+++ exercicio1/index.php
@@ -10,13 +10,13 @@
     <div class="bg-black">
         <div class="container">
             <h5>Exercício 01</h5>
-                <form action="index.php" method="post"> </form>
-                    <label for="first-number">Digite o primeiro número</label>
-                    <input type="text" name="first-number"><br>
-                    <label for="second-number">Digite o segundo número</label>
-                    <input type="text" name="second-number"><br>
-                    <input type="submit" value="enviar">
-                </form>
+            <form action="index.php" method="post">
+                <label for="first-number">Digite o primeiro número</label>
+                <input type="text" name="first-number"><br>
+                <label for="second-number">Digite o segundo número</label>
+                <input type="text" name="second-number"><br>
+                <input type="submit" value="enviar">
+            </form>
         </div>
     </div>
 </body>
