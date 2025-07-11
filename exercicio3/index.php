8cfc46d370bf2dd3704900298a6876ed4145c88b
jeancprado <jean.akaji@gmail.com>
2025-07-11 20:42:41 -0300

    exercício 3 frontend

---
+++ exercicio3/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Exercício 3</title>
+</head>
+
+<body>
+    <h4>Exercício III</h4>
+    <form action="index.php" method="post">
+        <div>
+            <label for="name">nome</label>
+            <input type="text" name="nome">
+
+            <label for="age">idade</label>
+            <input type="number" name="idade">
+
+            <label for="exeperience">Tempo de experiência profissional (em anos)</label>
+            <input type="number" name="experiencia">
+        </div>
+    </form>
+</body>
+</html>
