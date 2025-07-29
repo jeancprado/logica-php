a219f0f1ade1fec4e2f0dc3a4f719761e58c8da6
jeancprado <jean.akaji@gmail.com>
2025-07-29 20:19:46 -0300

    nvt-59 Exercício5 frontend

---
+++ exercicio5/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício V</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+
+        <h3>Qual é o formato do parelelepídedo?</h3> 
+        <p>insira as medidas abaixo para verificar</p> <br>
+
+        <form action="index.php" method="post">
+
+            <label for="comprimento">Comprimento:</label>
+            <input type="number" name="comprimento"> <br><br>
+
+            <label for="largura">Largura:</label>
+            <input type="number" name="largura"> <br><br>
+
+            <label for="altura">Altura:</label>
+            <input type="number" name="altura"> <br><br>
+
+            <button type="submit">Enviar</button> <br><br>
+
+        </form>
+
+    </div>
+</body>
+</html>
