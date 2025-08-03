b629457ea95df28e4474308fa736605eed220438
jeancprado <jean.akaji@gmail.com>
2025-08-03 16:03:11 -0300

    nvt-60 exercício 5 backend

--- exercicio5/index.php
+++ exercicio5/index.php
@@ -11,22 +11,40 @@
     <div class="container">
 
         <h3>Qual é o formato do parelelepídedo?</h3> 
-        <p>insira as medidas abaixo para verificar</p>
+        <p>insira as medidas abaixo para verificar</p> <br>
 
         <form action="index.php" method="post">
 
             <label for="comprimento">Comprimento:</label>
-            <input type="number" name="comprimento"> 
+            <input type="number" name="comprimento"> <br><br>
 
             <label for="largura">Largura:</label>
-            <input type="number" name="largura">
+            <input type="number" name="largura"> <br><br>
 
             <label for="altura">Altura:</label>
-            <input type="number" name="altura">
+            <input type="number" name="altura"> <br><br>
 
-            <button type="submit">Enviar</button>
+            <button type="submit">Enviar</button> <br><br>
 
         </form>
+
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $comprimento = $_POST['comprimento'];
+        $largura = $_POST['largura'];
+        $altura = $_POST['altura'];
+
+        if ($comprimento == $largura && $largura == $altura) {
+                echo $resultado = "Cubo";
+        } elseif ($comprimento == $largura || $comprimento == $altura || $largura == $altura) {
+                echo  $resultado = "Paralelepípedo Retangular";
+        } else {
+                echo $resultado = "Paralelepípedo Oblíquo";
+        }
+    }
+?>
 
     </div>
 </body>
