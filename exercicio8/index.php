9aa37ad7d4b864d2c62daff1a2df9dcd95767dbe
jeancprado <jean.akaji@gmail.com>
2025-09-15 23:44:49 -0300

    [nvt-86]-exercício8-Frontend

---
+++ exercicio8/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Exercício 8</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Contador e Soma</h1>
+        <form method="post">
+        <input type="number" name="limite" placeholder="Número limite" required>
+        <button type="submit">Calcular Soma</button>
+        </form>
+    </div>
+</body>
+</html>
