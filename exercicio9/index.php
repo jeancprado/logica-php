ff3e101a35047fc73d8fb8e1c7249250fca64384
jeancprado <jean.akaji@gmail.com>
2025-09-26 20:05:36 -0300

    [nvt-92] exercício 9 frontend

---
+++ exercicio9/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercício 9</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Repetidor de Palavra</h1>
+        <form method="post">
+            <input type="text" name="palavra" placeholder="Digite a palavra" required>
+            <input type="number" name="vezes" placeholder="Número de repetições" required>
+            <button type="submit">Repetir Palavra</button>
+        </form>
+    </div>
+</body>
+</html>
