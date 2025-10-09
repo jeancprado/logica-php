782d96d729793ea0e324f385df5953567c81ae02
jeancprado <jean.akaji@gmail.com>
2025-10-09 16:58:23 -0300

    aprendendo a criar funções com php

--- exercicio6/index.php
+++ exercicio6/index.php
@@ -1,10 +1,18 @@
+<?php
+    include "src/notaMusical.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+       $print = notaMusical($_POST["numero"]);
+    }
+$print7 = notaMusical(7);
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Exercício VI</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Exercício 6</title>
 </head>
 <body>
     <div class="container">
@@ -15,61 +23,15 @@
             <input type="number" id="numero" name="numero" min="1" max="10" required>
             <button type="submit">Enviar</button>
         </form>
+    
+        <div class="resultado">
+            <?= isset($print) ? $print : '' ?>
+        </div>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $numero = $_POST["numero"];
-        
-    if (is_numeric($numero)) {
-        $numero = (int)$numero;
-        $nota = "";
-
-        switch ($numero) {
-            case 1:
-                $nota = "Dó";
-                break;
-            case 2:
-                $nota = "Ré";
-                break;
-            case 3:
-                $nota = "Mi";
-                break;
-            case 4:
-                $nota = "Fá";
-                break;
-            case 5:
-                $nota = "Sol";
-                break;
-            case 6:
-                $nota = "Lá";
-                break;
-            case 7:
-                $nota = "Si";
-                break;
-            case 8:
-                $nota = "Dó#";
-                break;
-            case 9:
-                $nota = "Ré#";
-                break;
-            case 10:
-                $nota = "Fá#";
-                break;
-            default:
-                $nota = "Número inválido. Insira um número entre 1 e 10.";
-        }
-        
-        if ($nota) {
-            echo "<p class='resultado'>A nota musical correspondente é: " . $nota . "</p>";
-        } else {
-            echo "<p class='resultado'>" . $nota . "</p>";
-        }
-        
-    } else {
-        echo "<p class='resultado'>Por favor, insira um número válido.</p>";
-    }
-}
-?>
+        <div class="resultado">
+            <?= $print7 ?>
+        </div>
+    
     </div>
 </body>
 </html>
