00bdf1f94918da76ea18215a4db334c015945ca7
jeancprado <jean.akaji@gmail.com>
2025-12-26 14:30:37 -0300

    [nvt-197] exercicio 12 frontend

---
+++ exercicio12/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>exercicio12</title>
+</head>
+<body>
+    <div class="container">
+    <h1>Escada de Motivação</h1>
+
+    <form method="POST">
+        <label for="meta">Digite sua meta/objetivo:</label>
+        <input type="text" name="meta" id="meta" required>
+
+        <button type="submit">Gerar Escada</button>
+    </form>
+
+</div>
+</body>
+</html>
