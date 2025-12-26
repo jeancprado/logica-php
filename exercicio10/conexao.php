e0d400af10329cd7a86ca3fd7d2ed3a0ad315d88
jeancprado <jean.akaji@gmail.com>
2025-12-26 13:50:13 -0300

    [NVT-156] conectando banco de dados (exercicio10)

--- exercicio10/conexao.php
+++ exercicio10/conexao.php
@@ -8,7 +8,5 @@ $dbName = 'exercicio_dados';
 $conexao = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName);
 
 if (!$conexao) {
-    echo "Erro: " . mysqli_connect_error();
-} else {
-     echo "Conexão efetuada com sucesso"; 
+    die ("Erro Crítico de Conexão: " . mysqli_connect_error());
 }
