11819afaad28c174504606188e9c2c11f66d2588
jeancprado <jean.akaji@gmail.com>
2026-01-24 00:58:52 -0300

    enviando backend [nvt-205] e cypress

--- cypress/e2e/exercicio12.cy.js
+++ cypress/e2e/exercicio12.cy.js
@@ -5,8 +5,31 @@ describe('Exercício 12 - Escada da motivação', () => {
 
   it('Exibir o título da página, inputs e botão submit', () => {
     cy.contains('Escada de Motivação').should('be.visible');
-    cy.get('input[name="meta"]').should('be.visible')
+    cy.get('input[name="goal"]').should('be.visible')
     cy.get('button[type="submit"]').should('be.visible')
+    cy.contains('Histórico de Metas').should('be.visible');
+    cy.contains('Meta').should('be.visible');
+    cy.contains('Data e Hora').should('be.visible');
+
+  });
+
+  it('Adicionar palavra na escada de motivação', () => {
+    cy.get('input[name="goal"]').type('Titans')
+    cy.get('button[type="submit"]').click()
+  });
+  
+    it('Adicionar palavra na escada de motivação', () => {
+    cy.get('input[name="goal"]').type('Degrau')
+    cy.get('button[type="submit"]').click()
+  });
+
+    it('Testar se a escada funciona conforme o esperado', () => {
+    cy.get('input[name="goal"]').type('Agir')
+    cy.get('button[type="submit"]').click()
+    cy.contains('Agir').should('be.visible')
+    cy.contains('Agir Agir').should('be.visible')
+    cy.contains('Agir Agir Agir').should('be.visible')
+    cy.contains('Agir Agir Agir Agir').should('be.visible')
   });
   
 })
