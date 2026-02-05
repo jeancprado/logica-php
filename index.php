9a807d6e0f57926fe609fd23b7a02007420f717c
jeancprado <jean.akaji@gmail.com>
2026-02-05 20:04:20 -0300

    alterando interface do índice

--- index.php
+++ index.php
@@ -1,33 +1,107 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sumário</title>
+    <title>Sumário - Estilo Windows XP</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Sumário</h1>
-    <a href="exercicio1/">Acesse o exercício 1</a>
-    <a href="exercicio2/">Acesse o exercício 2</a>
-    <a href="exercicio3/">Acesse o exercício 3</a>
-    <a href="exercicio4/">Acesse o exercício 4</a>
-    <a href="exercicio5/">Acesse o exercício 5</a>
-    <a href="exercicio6/">Acesse o exercício 6</a>
-    <a href="exercicio7/">Acesse o exercício 7</a>
-    <a href="exercicio8/">Acesse o exercício 8</a>
-    <a href="exercicio9/">Acesse o exercício 9</a>
-    <a href="exercicio10/">Acesse o exercício 10</a>
-    <a href="exercicio11/">Acesse o exercício 11</a>
-    <a href="exercicio12/">Acesse o exercício 12</a>
-    <a href="exercicio13/">Acesse o exercício 13</a>
-    <a href="exercicio14/">Acesse o exercício 14</a>
-    <a href="exercicio15/">Acesse o exercício 15</a>
-    <a href="exercicio16/">Acesse o exercício 16</a>
-    <a href="exercicio17/">Acesse o exercício 17</a>
-    <a href="exercicio18/">Acesse o exercício 18</a>
-    <a href="exercicio19/">Acesse o exercício 19</a>
-    <a href="exercicio20/">Acesse o exercício 20</a>
+
+<div class="taskbar">
+    <div class="start-button">▶ Iniciar</div>
+</div>
+
+<div class="xp-window">
+
+    <div class="xp-titlebar">
+        📁 Sumário - Meus Exercícios
+    </div>
+
+    <div class="xp-content">
+
+        <a href="exercicio1/">
+             Exercício 1
+        </a>
+
+        <a href="exercicio2/">
+             Exercício 2
+        </a>
+
+        <a href="exercicio3/">
+             Exercício 3
+        </a>
+
+        <a href="exercicio4/">
+             Exercício 4
+        </a>
+
+        <a href="exercicio5/">
+             Exercício 5
+        </a>
+
+        <a href="exercicio6/">
+             Exercício 6
+        </a>
+
+        <a href="exercicio7/">
+             Exercício 7
+        </a>
+
+        <a href="exercicio8/">
+             Exercício 8
+        </a>
+
+        <a href="exercicio9/">
+             Exercício 9
+        </a>
+
+        <a href="exercicio10/">
+             Exercício 10
+        </a>
+
+        <a href="exercicio11/">
+             Exercício 11
+        </a>
+
+        <a href="exercicio12/">
+             Exercício 12
+        </a>
+
+        <a href="exercicio13/">
+             Exercício 13
+        </a>
+
+        <a href="exercicio14/">
+             Exercício 14
+        </a>
+
+        <a href="exercicio15/">
+             Exercício 15
+        </a>
+
+        <a href="exercicio16/">
+             Exercício 16
+        </a>
+
+        <a href="exercicio17/">
+             Exercício 17
+        </a>
+
+        <a href="exercicio18/">
+             Exercício 18
+        </a>
+
+        <a href="exercicio19/">
+             Exercício 19
+        </a>
+
+        <a href="exercicio20/">
+             Exercício 20
+        </a>
+
+    </div>
+</div>
 
 </body>
 </html>
